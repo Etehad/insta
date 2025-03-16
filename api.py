fed8510e5d25557d413a3e80fc5e11c53d6e738a
Etehad <etehad.taskforce1.4.1@gmail.com>
2025-03-16 17:11:42 +0330

    Update api.py

--- api.py
+++ api.py
@@ -1,5 +1,6 @@
 from flask import Flask
 import logging
+import os
 
 app = Flask(__name__)
 logger = logging.getLogger(__name__)
@@ -9,5 +10,6 @@ def ping():
     return "API is alive!", 200
 
 def start_api_server():
-    logger.info("Starting API server...")
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.getenv("PORT", 5000))  # Render.com پورت رو از متغیر PORT می‌گیره
+    logger.info(f"Starting API server on port {port}...")
+    app.run(host='0.0.0.0', port=port)
